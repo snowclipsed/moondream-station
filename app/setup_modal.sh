d31b1a45af79bf773c331690a4a51e87c5e2186e
snowclipsed <harrybishnoi@gmail.com>
2025-06-06 04:21:41 -0700

    setup modal

---
+++ app/setup_modal.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+modal shell --image ubuntu:20.04 --gpu L4 --cpu 4 --add-python 3.9 --cmd "
+apt update
+apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev git
+unset PYTHONPATH
+unset PYTHONHOME
+apt remove -y python3.9* || true
+rm -f /usr/bin/python3.9*
+rm -f /usr/local/bin/python3.9*
+rm -rf /usr/lib/python3.9*
+rm -rf /usr/local/lib/python3.9*
+rm -rf /install*
+rm -rf /pkg*
+
+# Add this - remove Modal's python3 symlink
+rm -f /usr/local/bin/python3
+rm -f /usr/local/bin/python
+
+cd /tmp
+wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz
+tar -xf Python-3.10.12.tgz
+cd Python-3.10.12
+./configure --enable-shared --enable-optimizations --prefix=/usr/local
+make -j\$(nproc)
+make altinstall
+echo '/usr/local/lib' > /etc/ld.so.conf.d/python310.conf
+ldconfig
+rm -f /usr/bin/python*
+rm -f /usr/bin/pip*
+
+# Create fresh symlinks in /usr/local/bin first
+ln -sf /usr/local/bin/python3.10 /usr/local/bin/python3
+ln -sf /usr/local/bin/python3.10 /usr/local/bin/python
+
+ln -sf /usr/local/bin/python3.10 /usr/bin/python3.10
+ln -sf /usr/local/bin/python3.10 /usr/bin/python3
+ln -sf /usr/local/bin/python3.10 /usr/bin/python
+export PATH=/usr/local/bin:\$PATH
+export PYTHONPATH=
+export PYTHONHOME=/usr/local
+echo 'export PATH=/usr/local/bin:\$PATH' >> ~/.bashrc
+echo 'export PYTHONPATH=' >> ~/.bashrc
+echo 'export PYTHONHOME=/usr/local' >> ~/.bashrc
+/usr/local/bin/python3.10 -m ensurepip
+ln -sf /usr/local/bin/pip3.10 /usr/bin/pip3
+ln -sf /usr/local/bin/pip3.10 /usr/bin/pip
+pip install pyinstaller distro certifi
+cd ~
+git clone https://github.com/snowclipsed/moondream-station/
+python --version
+pip --version
+exec bash
+"
