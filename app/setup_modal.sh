4b743c91c50e72868d249049159f9231f4f35126
snowclipsed <harrybishnoi@gmail.com>
2025-06-06 17:14:30 -0700

    remove checking if repo exists. always rebuild.

--- app/setup_modal.sh
+++ app/setup_modal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-modal shell --image ubuntu:20.04 --gpu L4 --cpu 4 --add-python 3.9 --volume moondream-vol --cmd "
+modal shell --image ubuntu:20.04 --gpu A10G --add-python 3.9 --volume moondream-vol --cmd "
 apt update
 apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev git
 unset PYTHONPATH
@@ -41,23 +41,29 @@ ln -sf /usr/local/bin/pip3.10 /usr/bin/pip
 pip install pyinstaller distro certifi
 
 cd /mnt/moondream-vol
-if [ ! -d moondream-station ]; then
-  git clone https://github.com/snowclipsed/moondream-station/
-fi
+rm -rf moondream-station
+git clone https://github.com/snowclipsed/moondream-station/
 
-cd moondream-station/app
-bash build.sh dev ubuntu --build-clean
-cd /mnt/moondream-vol/moondream-station
+cd /mnt/moondream-vol/moondream-station/
 rm -rf MoondreamStation.tar
 rm -rf moondream_station_executable.tar
-cd /mnt/moondream-vol/moondream-station/output
+git pull
+ls
 
+cd /mnt/moondream-vol/moondream-station/app
+bash build.sh dev ubuntu --build-clean
+cd /mnt/moondream-vol/moondream-station
+ls
+
+cd /mnt/moondream-vol/moondream-station/output
 tar -cvf moondream_station_executable.tar moondream_station/
 cp moondream_station_executable.tar /mnt/moondream-vol/moondream-station/
+ls
 
 cd /root/.local/share/
 tar -cvf MoondreamStation.tar MoondreamStation/
 cp MoondreamStation.tar /mnt/moondream-vol/moondream-station/
+ls
 
 python --version
 pip --version
