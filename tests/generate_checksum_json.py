b25f44071572d364ec26334a1812775a514fe2d3
snowclipsed <harrybishnoi@gmail.com>
2025-06-17 15:04:26 -0700

    add ignore extension functionality

--- tests/generate_checksum_json.py
+++ tests/generate_checksum_json.py
@@ -4,6 +4,7 @@ import hashlib
 
 DEFAULT_IGNORE_DIRS = {'.git', '.venv', '__pycache__', 'node_modules', '.pytest_cache', 'py_versions'}
 DEFAULT_IGNORE_FILES = {'__init__.py', '.DS_Store', 'Thumbs.db', '.gitignore'}
+DEFAULT_IGNORE_EXTENSIONS = {'.log'}
 
 def get_md5(filepath):
     with open(filepath, 'rb') as f:
@@ -12,9 +13,13 @@ def get_md5(filepath):
 def should_ignore(name, ignore_set):
     return name in ignore_set or any(name.endswith(pattern.lstrip('*')) for pattern in ignore_set if '*' in pattern)
 
-def generate_json(root_path, ignore_dirs=None, ignore_files=None, output_file=None):
+def should_ignore_extension(filepath, ignore_extensions):
+    return any(filepath.endswith(ext) for ext in ignore_extensions)
+
+def generate_json(root_path, ignore_dirs=None, ignore_files=None, ignore_extensions=None, output_file=None):
     ignore_dirs = ignore_dirs or DEFAULT_IGNORE_DIRS
     ignore_files = ignore_files or DEFAULT_IGNORE_FILES
+    ignore_extensions = ignore_extensions or DEFAULT_IGNORE_EXTENSIONS
     
     result = {}
     
@@ -26,7 +31,7 @@ def generate_json(root_path, ignore_dirs=None, ignore_files=None, output_file=No
             rel_root = ''
         
         for file in files:
-            if should_ignore(file, ignore_files):
+            if should_ignore(file, ignore_files) or should_ignore_extension(file, ignore_extensions):
                 continue
                 
             rel_path = os.path.join(rel_root, file) if rel_root else file
@@ -57,15 +62,19 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', help='Output JSON file')
     parser.add_argument('--ignore-dirs', nargs='*', help='Additional directories to ignore')
     parser.add_argument('--ignore-files', nargs='*', help='Additional files to ignore')
+    parser.add_argument('--ignore-extensions', nargs='*', help='File extensions to ignore (e.g., .log .tmp)')
     
     args = parser.parse_args()
     
     ignore_dirs = DEFAULT_IGNORE_DIRS.copy()
     ignore_files = DEFAULT_IGNORE_FILES.copy()
+    ignore_extensions = DEFAULT_IGNORE_EXTENSIONS.copy()
     
     if args.ignore_dirs:
         ignore_dirs.update(args.ignore_dirs)
     if args.ignore_files:
         ignore_files.update(args.ignore_files)
+    if args.ignore_extensions:
+        ignore_extensions.update(args.ignore_extensions)
     
-    generate_json(args.directory, ignore_dirs, ignore_files, args.output)
+    generate_json(args.directory, ignore_dirs, ignore_files, ignore_extensions, args.output)
